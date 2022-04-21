3fbe1180210432d5a087c74a6404c2c14dd7b51b
Vishal Sharma <vishallsharma07@gmail.com>
2022-04-21 17:26:18 +0530

    update

--- aws-node-rest-api-project/src/user/route.js
+++ aws-node-rest-api-project/src/user/route.js
@@ -1,3 +1,8 @@
+var AWS = require("aws-sdk");
+AWS.config.update({region: "us-east-1"});
+var dynamodb = new AWS.DynamoDB({apiVersion: "2012-08-10"});
+
+
 const express = require('express')
 const routes = express.Router({mergeParams : true})
 
@@ -5,4 +10,22 @@ routes.get('/',(req, res) =>{
     res.status(200).json({message : "Lambda Working"})
 })
 
+routes.get('/getData',(req, res) =>{
+    async function logSingleItem(){
+        try {
+            var params = {
+                Key: {
+                 "artist": {"S": "test name"}, 
+                 "song": {"S": "test song"}
+                }, 
+                TableName: "basicSongsTable"
+            };
+            var result = await dynamodb.getItem(params).promise()
+            res.status(200).json({ data : result})
+        } catch (error) {
+            res.status(403).json({ message : error.message})
+        }
+    }
+    logSingleItem()
+})
 module.exports = routes
