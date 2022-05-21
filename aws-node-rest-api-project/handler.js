a9c1a2f9488e8071b2c883c3dff8a350608357c5
Vishal Sharma <vishallsharma07@gmail.com>
2022-05-21 13:22:06 +0530

    serverless with dynamodb

--- aws-node-rest-api-project/handler.js
+++ aws-node-rest-api-project/handler.js
@@ -3,4 +3,4 @@ const app = require('./src/index')
 const server = awsServerlessExpress.createServer(app)
 exports.handler = (event, context) =>{
   return awsServerlessExpress.proxy(server, event, context)
-}
+} // due to this getting some time error internal server error but now working fine
